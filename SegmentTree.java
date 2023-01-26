1737955c976aa1cb9899cba81d6d49eb5175c22e
Davide <davide9706@gmail.com>
2023-01-26 20:14:03 +0100

    REF again and persistency added

--- SegmentTree.java
+++ SegmentTree.java
@@ -1,15 +1,31 @@
 import static java.lang.Math.min;
 import static java.lang.Math.max;
-	    
+
 class Node <T> {
-	int l;
-	int r;
+	int firstInPos;
+	int lastInPos;
 	T value;
+	T pushDown;
+	Node <T> leftNode;
+	Node <T> rightNode;
 		
-	public Node(int l, int r, T value) {
-		this.l = l;
-		this.r = r;
+	public Node(int fi, int li, T value) {
+		this.firstInPos = fi;
+		this.lastInPos = li;
 		this.value = value;
+	}
+	
+	public Node <T> clone() {
+		Node <T> node = new Node <T> (firstInPos, lastInPos, value);
+		node.leftNode = leftNode;
+		node.rightNode = rightNode;
+		node.pushDown = pushDown;
+		return node;
+	}
+	
+	@Override
+	public String toString() {
+		return "( [" + firstInPos + ", " + lastInPos + "] | value: " + value + ", pushDown: " + pushDown + " )";
 	}
 }
 
@@ -34,84 +50,98 @@ interface RangeFunction <T> {
 }
 
 class SegmentTree <T> {
-	Node <T>[] tree;
-	T[] leaves;
-	T[] pushDown;
+	public final int FIRST_VERSION = 0;
+	
+	List<Node <T>> roots;
 	ComputeFunction <T> compute;
 	ModifyFunction <T> modify;
 	RangeFunction <T> range;
 	
-	public SegmentTree(T[] leaves, ComputeFunction <T> f1, ModifyFunction <T> f2, RangeFunction <T> f3) {
-		this.leaves = leaves;
-		this.tree = new Node[this.leaves.length * 4];
-		this.pushDown = (T[]) new Object[this.leaves.length * 4];
-		this.compute = f1;
-		this.modify = f2;
-		this.range = f3;
-		this.build(0, this.leaves.length - 1, 0);
+	public SegmentTree(T[] leaves, ComputeFunction <T> compute, ModifyFunction <T> modify, RangeFunction <T> range) {
+		final int N = leaves.length;
+		this.compute = compute;
+		this.modify = modify;
+		this.range = range;
+		roots = new ArrayList <> ();
+		roots.add(build(0, N - 1, leaves));
 	}
-
-	private void build(int l, int r, int index) {
-		if(l == r) {
-			tree[index] = new Node <T> (l, r, leaves[l]);
+	
+	private Node <T> build(int l, int r, T[] leaves) {
+		if(l == r) 
+			return new Node <T> (l, r, leaves[l]);
+		Node <T> curr = new Node <T> (l, r, null);
+		curr.leftNode = this.build(l, (l + r) >> 1, leaves);
+		curr.rightNode = this.build(((l + r) >> 1) + 1, r, leaves);
+		curr.value = compute.compute(curr.leftNode.value, curr.rightNode.value);
+		return curr;
+	}
+	
+	private void pushDown(Node <T> node) {
+		if(node.pushDown == null)
 			return;
-		}
-		this.build(l, (l + r) >> 1, (index << 1) + 1);
-		this.build(((l + r) >> 1) + 1, r, (index << 1) + 2);
-		tree[index] = new Node <T> (l, r, compute.compute(tree[(index << 1) + 1].value, tree[(index << 1) + 2].value));
+		node.leftNode = update(node.leftNode, node.firstInPos, (node.firstInPos + node.lastInPos) >> 1, node.pushDown);
+		node.rightNode = update(node.rightNode, ((node.firstInPos + node.lastInPos) >> 1) + 1, node.lastInPos, node.pushDown);
+		node.pushDown = null;
 	}
 		
-	private void update(int index, int l, int r, T value) {
-		if(l > r)
-			return;
-		if(tree[index] == null)
-			return;
-		if(tree[index].l == tree[index].r) {
-			tree[index].value = leaves[tree[index].l] = modify.modify(leaves[tree[index].l], value);
-			return;
-		}
-		if(tree[index].l == l && tree[index].r == r) {
-			pushDown[index] = modify.modify(pushDown[index], value);
-			tree[index].value = modify.modify(tree[index].value, range.range(value, tree[index].l, tree[index].r));
-			return;
-		}
-		if(pushDown[index] != null) 
-			pushDown(index);
-		if(l <= (tree[index].l + tree[index].r) >> 1) 
-			update((index << 1) + 1, l, min(r, (tree[index].l + tree[index].r) >> 1), value);
-		if(r >= ((tree[index].l + tree[index].r) >> 1) + 1) 
-			update((index << 1) + 2, max(l, ((tree[index].l + tree[index].r) >> 1) + 1), r, value);
-		tree[index].value = compute.compute(tree[(index << 1) + 1].value, tree[(index << 1) + 2].value);
-	}
-	
-	private void pushDown(int index) {
-		update((index << 1) + 1, tree[index].l, (tree[index].l + tree[index].r) >> 1, pushDown[index]);
-		update((index << 1) + 2, ((tree[index].l + tree[index].r) >> 1) + 1, tree[index].r, pushDown[index]);
-		pushDown[index] = null;
-	}
-	
-	public void update(int l, int r, T value) {
-		update(0, l, r, value);
-	}
-	
-	private T query(int index, int l, int r) {
+	private Node <T> update(Node <T> oldNode, int l, int r, T value) {
 		if(l > r)
 			return null;
-		if(tree[index] == null)
+		if(oldNode == null)
+			oldNode = new Node <> (l, r, null);
+    			Node <T> newNode = oldNode.clone();
+		if(newNode.firstInPos == newNode.lastInPos) {
+			newNode.value = modify.modify(newNode.value, value);
+			return newNode;
+		}
+		if(newNode.firstInPos == l && newNode.lastInPos == r) {
+			newNode.value = modify.modify(newNode.value, range.range(value, newNode.firstInPos, newNode.lastInPos));
+			newNode.pushDown = modify.modify(newNode.pushDown, value);
+			return newNode;
+		}
+		pushDown(newNode);
+		if(l <= (newNode.firstInPos + newNode.lastInPos) >> 1) 
+			newNode.leftNode = update(newNode.leftNode, l, min(r, (newNode.firstInPos + newNode.lastInPos) >> 1), value);
+		if(r >= ((newNode.firstInPos + newNode.lastInPos) >> 1) + 1) 
+			newNode.rightNode = update(newNode.rightNode, max(l, ((newNode.firstInPos + newNode.lastInPos) >> 1) + 1), r, value);
+		newNode.value = compute.compute(newNode.leftNode != null ? newNode.leftNode.value : null, newNode.rightNode != null ? newNode.rightNode.value : null);
+		return newNode;
+	}
+	
+	public int update(int version, int l, int r, T value) {
+		roots.add(update(roots.get(version), l, r, value));
+		return lastVersion();
+	}
+	
+	public int update(int l, int r, T value) {
+		return update(lastVersion(), l, r, value);
+	}
+	
+	private T query(Node <T> curr, int l, int r) {
+		if(l > r)
 			return null;
-		if(pushDown[index] != null) 
-			pushDown(index);
-		if(tree[index].l == l && tree[index].r == r)
-			return tree[index].value;
+		if(curr == null)
+			return null;
+		pushDown(curr);
+		if(curr.firstInPos == l && curr.lastInPos == r)
+			return curr.value;
 		T ans = null;
-		if(l <= (tree[index].l + tree[index].r) >> 1) 
-			ans = compute.compute(ans, query((index << 1) + 1, l, min(r, (tree[index].l + tree[index].r) >> 1)));
-		if(r >= ((tree[index].l + tree[index].r) >> 1) + 1)
-			ans = compute.compute(ans, query((index << 1) + 2, max(l, ((tree[index].l + tree[index].r) >> 1) + 1), r));
+		if(l <= (curr.firstInPos + curr.lastInPos) >> 1) 
+			ans = compute.compute(ans, query(curr.leftNode, l, min(r, (curr.firstInPos + curr.lastInPos) >> 1)));
+		if(r >= ((curr.firstInPos + curr.lastInPos) >> 1) + 1)
+			ans = compute.compute(ans, query(curr.rightNode, max(l, ((curr.firstInPos + curr.lastInPos) >> 1) + 1), r));
 		return ans;
 	}
 	
+	public T query(int version, int l, int r) {
+		return query(roots.get(version), l, r);
+	}
+	
 	public T query(int l, int r) {
-		return query(0, l, r);
+		return query(lastVersion(), l, r);
+	}
+	
+	public int lastVersion() {
+		return roots.size() - 1;
 	}
 }
